6c19f6a7d5ebc1ce98f8af0b3023fd1b566dcee8
Victor Alfonso <114443508+victorgg93@users.noreply.github.com>
2024-02-06 18:57:15 -0500

    correcciones

---
+++ models/user.js
@@ -0,0 +1,39 @@
+// user.js
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user' // Valor por defecto para los usuarios comunes
+    }
+});
+
+
+
+// Middleware para cifrar la contraseña antes de guardarla
+userSchema.pre('save', async function (next) {
+    const user = this;
+    if (user.isModified('password')) {
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt);
+    }
+    next();
+});
+
+// Método para comparar contraseñas
+userSchema.methods.comparePassword = async function (candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
